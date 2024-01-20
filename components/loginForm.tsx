5aab8b6bae665de41b05f994851bb564db64f5c6
Islem Maboud <mypersonal-email@gmail.com>
2024-01-20 09:47:30 +0100

    Allow to login & authentication users through the login form

--- components/loginForm.tsx
+++ components/loginForm.tsx
@@ -1,5 +1,28 @@
+"use client";
+
+import authService from "@/services/authService";
+import { useRouter } from "next/router";
+import { useState } from "react";
+
 export function LoginForm() {
-  const handleLogin = () => {};
+  const [isLoading, setLoading] = useState(false);
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+
+  const router = useRouter();
+
+  const handleLogin = async () => {
+    setLoading(true);
+
+    try {
+      await authService.login(email, password);
+      router.push("/dashboard");
+    } catch (err) {
+      router.push("/login");
+    }
+
+    setLoading(false);
+  };
 
   return (
     <section className="bg-gray-50 dark:bg-gray-900">
@@ -18,7 +41,7 @@ export function LoginForm() {
         <div className="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
           <div className="p-6 space-y-4 md:space-y-6 sm:p-8">
             <h1 className="text-xl font-bold leading-tight tracking-tight text-gray-900 md:text-2xl dark:text-white">
-              Sign in to your account
+              Sign up for a new account
             </h1>
             <form className="space-y-4 md:space-y-6" onSubmit={handleLogin}>
               <div>
--- services/authService.ts
+++ services/authService.ts
@@ -22,6 +22,30 @@ class AuthService {
       return new Error("Signup failed");
     }
   }
+
+  async login(email: string, password: string) {
+    try {
+      const response = await fetch("/api/login", {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ email, password }),
+      });
+
+      if (response.ok) {
+        // Redirect to a success page or login page
+        return await response.json();
+      } else {
+        // Handle signup error
+        console.error("Login failed");
+        return new Error("Wrong email or password");
+      }
+    } catch (error) {
+      console.error("Error during login:", error);
+      return new Error("Wrong email or password");
+    }
+  }
 }
 
 export default new AuthService();
