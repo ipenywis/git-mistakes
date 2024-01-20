0396ddc9dcec11db01f187776b18db1839564075
Islem Maboud <mypersonal-email@gmail.com>
2024-01-20 09:37:19 +0100

    Add login route with db credentials matching

--- utils/db.ts
+++ utils/db.ts
@@ -1,6 +1,10 @@
 export default {
   createUser(...params: any[]): any {},
 
+  matchUserWithPassword(email: string, password: string): any {
+    return {};
+  },
+
   get(): any {
     return {};
   },
